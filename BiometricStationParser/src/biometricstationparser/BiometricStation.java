09bf4ec1bb42f304d53cbb3864326bd8d98b8fa4
jasper debaene <jasperdebaene@hotmail.be>
2018-05-02 14:28:02 +0200

    here ya go Arne, have fun testing!

---
+++ BiometricStationParser/src/biometricstationparser/BiometricStation.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package biometricstationparser;
+
+import com.fazecast.jSerialComm.SerialPort;
+import com.fazecast.jSerialComm.SerialPortDataListener;
+import com.fazecast.jSerialComm.SerialPortEvent;
+
+/**
+ *
+ * @author Jasper
+ */
+public class BiometricStation {
+
+    public static void main(String[] args) {
+
+        int MAX_BYTES = 128;
+        SerialPort comPort = SerialPort.getCommPorts()[0];
+        comPort.openPort();
+        comPort.addDataListener(new SerialPortDataListener() {
+            @Override
+            public int getListeningEvents() {
+                return SerialPort.LISTENING_EVENT_DATA_AVAILABLE;
+            }
+
+            @Override
+            public void serialEvent(SerialPortEvent event) {
+                if (event.getEventType() != SerialPort.LISTENING_EVENT_DATA_AVAILABLE) {
+                    return;
+                }
+                byte[] newData = new byte[MAX_BYTES];
+                char[] message = new char[MAX_BYTES];
+                int numRead = comPort.readBytes(newData, newData.length);
+                //System.out.println("Read " + numRead + " bytes.");
+
+                for (int i = 0; i < newData.length; i++) {
+                    message[i] = (char) (newData[i]);
+                }
+
+                String output = new String(newData);
+                System.out.println("Data: " + output);
+                if (output.contains("]")) {
+                    System.out.println("Found newline");
+                }
+            }
+        });
+
+        SensorData data = new SensorData(25.0, 80, 1, 2, 3);
+
+        System.out.println(data);
+
+        BiometricStationStringParser parser = new BiometricStationStringParser();
+        SensorData testData = parser.parse("[25.2|80|1,2,3]");
+        System.out.println(testData);
+
+        //Bad data
+        SensorData badData = parser.parse("[25.2|80|1,2,3]");
+        System.out.println(badData);
+    }
+
+}
---
+++ BiometricStationParser/src/biometricstationparser/BiometricStationStringParser.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package biometricstationparser;
+
+/**
+ *
+ * @author Jasper
+ */
+public class BiometricStationStringParser {
+
+    public SensorData parse(String dataString) {
+
+        if (!isValidString(dataString)) {
+            return null;
+        }
+        double temperature = 0;
+        int hearthbeat = 0;
+        int coordinate_x = 0;
+        int coordinate_y = 0;
+        int coordinate_z = 0;
+        String test = "[25.2|80|1,2,3]";
+
+        //parsen
+        
+        
+        return new SensorData(temperature, hearthbeat, coordinate_x, coordinate_y, coordinate_z);
+    }
+
+    private boolean isValidString(String dataString) {
+//        if (dataString.indexOf("|") != -1
+//                && dataString.indexOf("|") != -1
+//                && dataString.indexOf("|") != -1) {
+//            return true;
+//        }   else {
+//            return false;
+//        }
+//    }
+
+        return (dataString.indexOf("|") != -1
+                && dataString.indexOf("|") != -1
+                && dataString.indexOf("|") != -1);
+    }
+}
